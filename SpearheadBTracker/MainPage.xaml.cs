6d1035854462217143a2f92bbe3356ff1e6d5078
PugDLife <dewaldrautenbach1@gmail.com>
2025-09-15 18:46:06 +0200

    Add project files.

---
+++ SpearheadBTracker/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SpearheadBTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SpearheadBTracker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SpearheadBTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
